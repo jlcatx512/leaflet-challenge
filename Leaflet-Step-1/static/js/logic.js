b6d9f27a1fcf35d35864f35df606738223a2c3f9
Jadd Cheng <52934066+jlcatx512@users.noreply.github.com>
2019-11-19 10:54:48 -0600

    Added legend but need to fix styling for it.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,14 +1,6 @@
 // Jadd Cheng
 // November 17, 2019
 
-// NB this code is broken up in to two pars.
-// First, the functions that make up the immediately invoked function.
-// Second, the immediately invoked function. --> await call because must wait for data to load.
-
-// Function create features from earthquakeData.
-// NB earthquakeData --> data.features.
-// The original data response was geoJSON FeatureCollections.
-// Need to extract the features key only.
 
 function createEarthquakeFeatures(earthquakeData) {
 
@@ -62,11 +54,6 @@ function createEarthquakeFeatures(earthquakeData) {
         return L.circleMarker(latlng, geojsonMarkerOptions);
     }
 
-    // const faultlines = L.geoJSON(tectonicData, {
-        
-    //     onEachFeature: onEachFeature
-
-    // });
     const earthquakes = L.geoJSON(earthquakeData, {
     
         pointToLayer: createCircleMarker,
@@ -87,7 +74,7 @@ function createFaultlineFeatures(faultlinesData) {
 
 function createMap(earthquakes, faultlines) {
 
-    // Define streetmap and darkmap layers
+    // Define satellite, streetmap and darkmap layers
     const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
             attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
             maxZoom: 18,
@@ -135,12 +122,12 @@ function createMap(earthquakes, faultlines) {
     L.control.layers(baseMaps, overlayMaps, {
             collapsed: false
     }).addTo(myMap);
-}
 
-// ADD LEGEND
-var legend = L.control({position: 'bottomright'});
-legend.onAdd = function (map) {
+    const legend = L.control({position: "bottomright"});
+
+    legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend'),
+    // var div = L.DomUtil.create('div', 'legend'),
         grades = [0, 1, 2, 3, 4, 5],
         labels = [];
 
@@ -150,15 +137,59 @@ legend.onAdd = function (map) {
             '<i style="background:' + circleColor(grades[i] + 1) + '"></i> ' +
             grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
-
     return div;
+    };
+
+    legend.addTo(myMap);
+}
+
+// ADD LEGEND
+
+
+function circleColor(d) {
+    // return d > 5 ? '#800026' :
+    return d > 5 ? 'red' :
+            // d > 4  ? '#BD0026' :
+            d > 4  ? 'pink' :
+            // d > 3  ? '#E31A1C' :
+            d > 3  ? 'yellow' :
+            // d > 2  ? '#FC4E2A' :
+            d > 2  ? 'orange' :
+            // d > 1   ? '#FD8D3C' :
+            d > 1   ? 'lime' :
+            // d > 0   ? '#FEB24C' :
+            // d > 0   ? '#00ff00' :
+            d > 0   ? 'green' :
+        // d > 0   ? '#FED976' :
+                    '#FFEDA0';
+    // return "#ff7800"
+}
+
+function createLegend (map) {
+    const legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+    // var div = L.DomUtil.create('div', 'legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + circleColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+    };
+
+    legend.addTo(myMap);
 };
-// legend.addTo(map);
 
 // IMMEDIATELY INVOKED Function.
 // This is what runs when the page loads.
 // LOGIC OF THE CODE.
-// const queryUrl = buildUrl();
+
 (async function(){
     const earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
     const faultlinesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
