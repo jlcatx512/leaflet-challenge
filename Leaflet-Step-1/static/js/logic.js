7f292161823a8a1c1ded579495de8b17caad550f
Jadd Cheng <52934066+jlcatx512@users.noreply.github.com>
2019-11-18 21:34:44 -0600

    Added tectonic plates fault lines overlay map. Still need to add legend.

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,14 +1,21 @@
 // Jadd Cheng
 // November 17, 2019
 
+// NB this code is broken up in to two pars.
+// First, the functions that make up the immediately invoked function.
+// Second, the immediately invoked function. --> await call because must wait for data to load.
+
 // Function create features from earthquakeData.
-function createFeatures(earthquakeData) {
+// NB earthquakeData --> data.features.
+// The original data response was geoJSON FeatureCollections.
+// Need to extract the features key only.
+
+function createEarthquakeFeatures(earthquakeData) {
 
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
-        console.log(feature)
-        // layer.circle()
+        // console.log(feature)
         layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     }
@@ -16,44 +23,69 @@ function createFeatures(earthquakeData) {
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     // earthquakeData --> data.features
+    function circleColor(d) {
+        // return d > 5 ? '#800026' :
+        return d > 5 ? 'red' :
+                // d > 4  ? '#BD0026' :
+                d > 4  ? 'pink' :
+                // d > 3  ? '#E31A1C' :
+                d > 3  ? 'yellow' :
+                // d > 2  ? '#FC4E2A' :
+                d > 2  ? 'orange' :
+                // d > 1   ? '#FD8D3C' :
+                d > 1   ? 'lime' :
+                // d > 0   ? '#FEB24C' :
+                // d > 0   ? '#00ff00' :
+                d > 0   ? 'green' :
+            // d > 0   ? '#FED976' :
+                        '#FFEDA0';
+        // return "#ff7800"
+    }
 
     // create Circle marker function. to put in radius option.
     function circleSize(feature){
         magnitude = parseFloat(feature.properties.mag)
-        console.log(magnitude)
+        // console.log(magnitude)
         return magnitude
-    }
-
-    function circleColor(feature, magnitude) {
-        pass
     }
 
     function createCircleMarker(feature, latlng) {
         let geojsonMarkerOptions = {
-            // radius: 30,
-            radius: feature.properties.mag*15,
-            fillColor: "#ff7800",
-            // fillColor: feature.properties.mag,
+            radius: feature.properties.mag*5,
+            fillColor: circleColor(feature.properties.mag),
             color: "#000",
             weight: 1,
             opacity: 1,
-            fillOpacity: 0.8
+            // fillOpacity: 0.8
+            fillOpacity: 1.2
         }
         return L.circleMarker(latlng, geojsonMarkerOptions);
     }
 
-    const earthquakes = L.geoJSON(earthquakeData, {
+    // const faultlines = L.geoJSON(tectonicData, {
         
+    //     onEachFeature: onEachFeature
+
+    // });
+    const earthquakes = L.geoJSON(earthquakeData, {
+    
         pointToLayer: createCircleMarker,
         onEachFeature: onEachFeature
-
+    
     });
 
     // Sending our earthquakes layer to the createMap function
-    createMap(earthquakes);
+    // createMap(earthquakes);
+    return earthquakes;
 }
 
-function createMap(earthquakes) {
+// Create faultlines geoJSON layer.
+function createFaultlineFeatures(faultlinesData) {
+    const faultlines = L.geoJSON(faultlinesData)
+    return faultlines
+}
+
+function createMap(earthquakes, faultlines) {
 
     // Define streetmap and darkmap layers
     const streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -69,23 +101,32 @@ function createMap(earthquakes) {
             id: "mapbox.dark",
             accessToken: API_KEY
     });
+ 
+    const satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "mapbox.satellite",
+            accessToken: API_KEY
+    });
 
     // Define a baseMaps object to hold our base layers
     const baseMaps = {
-            "Street Map": streetmap,
-            "Dark Map": darkmap
+            "Satellite": satellitemap,   
+            "Dark Map": darkmap,
+            "Street Map": streetmap
     };
 
     // Create overlay object to hold our overlay layer
     const overlayMaps = {
-            Earthquakes: earthquakes
+            Earthquakes: earthquakes,
+            "Fault Lines": faultlines
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
     const myMap = L.map("map", {
             center: [37.09, -95.71],
             zoom: 5,
-            layers: [streetmap, earthquakes]
+            layers: [satellitemap, faultlines, earthquakes] // layers shown by default.
     });
 
     // Create a layer control
@@ -96,24 +137,39 @@ function createMap(earthquakes) {
     }).addTo(myMap);
 }
 
-// (async function(){
-//     // earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
-//     // earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-//     // earthquakeURL = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-//     // const queryUrl = buildUrl();
-//     const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-//     console.log("The geojson source is " + queryUrl)
-//     const data = await d3.json(queryUrl);
-//     console.log(data)
-//     // Once we get a response, send the data.features object to the createFeatures function
-//     createFeatures(data.features);
-// })()
+// ADD LEGEND
+var legend = L.control({position: 'bottomright'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + circleColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+// legend.addTo(map);
+
+// IMMEDIATELY INVOKED Function.
+// This is what runs when the page loads.
+// LOGIC OF THE CODE.
+// const queryUrl = buildUrl();
 (async function(){
-    // const queryUrl = buildUrl();
-    // const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-    const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
-    const data = await d3.json(queryUrl);
-    console.log(data);
+    const earthquakeUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    const faultlinesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+    // const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+    const earthquakeData = await d3.json(earthquakeUrl);
+    const faultlinesData = await d3.json(faultlinesUrl);
+    // console.log(data);
+
     // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
+    var earthquakes = createEarthquakeFeatures(earthquakeData.features);
+    var faultlines = createFaultlineFeatures(faultlinesData.features);
+
+    createMap(earthquakes, faultlines);
 })()
--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # leaflet-challenge
 November 17, 2019
+
+## ACTION ITEMS ##
+- [X] Leaflet tutorials
+- [X] Go through class activities.
+- [X] https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson
+    * Start with a small set first.
+- All Earthquakes past day is good. About 358? occurences.
+- [ ] Add input dropdown menu to select dataset?
+
+- [ ] https://github.com/timwis/leaflet-choropleth/blob/gh-pages/src/choropleth.js
+
+## Drop-down List ##
+* Take the selected link as the input.
+* Pass the input to URL.
+* Have a default.
+
+## Structure 
+* Rewatching the videos help.
+* immediately invoked function --> keystone
+* define the functions above it.
+* Need to GET the json from GeoJSON.
+* L.geoJSON method takes an array of features. 
+* But to get that array of features you need to import the geoJSON string.
+* to access only the features part, you need to pass the string to a JSON parser.
+* In this case this is d3.
+* the point will be auto-generated from L.geoJSON.
+* NB subtle bugs in the subtle codes. Better off starting from scratch or copying from one of the Solved classroom activities.
+
+## MUST READ ##
+* http://127.0.0.1:5501/1/Activities/10-Stu_Geo-Json/Solved/
+* [Main list of feeds](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+* A `Feature` is a spacially bounded space, e.g. 
+* A `FeatureCollection` is a list of features.
+* [GeoJSON Summary Format](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
+> GeoJSON is a format for encoding a variety of geographic data structures. A GeoJSON object may represent a geometry, a feature, or a collection of features. GeoJSON uses the JSON standard. The GeoJSONP feed uses the same JSON response, but the GeoJSONP response is wrapped inside the function call, eqfeed_callback. See the GeoJSON site for more information.
+* [Past 30 Days Updated every minute. All Earthquakes](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson)
+* 
+
+```
+L.geoJSON(data, {
+    style: function (feature) {
+        return {color: feature.properties.color};
+    }
+}).bindPopup(function (layer) {
+    return layer.feature.properties.description;
+}).addTo(map);`
+``` 
+
+> GeoJSON is a format for encoding a variety of geographic data structures […]. A GeoJSON object may represent a region of space (a Geometry), a spatially bounded entity (a Feature), or a list of Features (a FeatureCollection). GeoJSON supports the following geometry types: Point, LineString, Polygon, MultiPoint, MultiLineString, MultiPolygon, and GeometryCollection. Features in GeoJSON contain a Geometry object and additional properties, and a FeatureCollection contains a list of Features.
+
+## FLOW ##
+| Method | Argument | Example   |
+| ---   | ---   | ---   |
+|`L.map.()` | `div` element |     |
+|`L.tileLayer()`| takes URL template. access token. id, zoom level | xx |
+|`L.addTo()`| takes map `L.map` map object as argument. Usually chained to `L.tileLayer()` | |
+
+## MapBox Tile Styles ##
+`mapbox.satellite`
+
+## Attributions ##
+* [Leaflet.js](https://leafletjs.com/examples/quick-start/)
+> Whenever using anything based on OpenStreetMap, an attribution is obligatory as per the copyright notice. Most other tile providers (such as MapBox, Stamen or Thunderforest) require an attribution as well. Make sure to give credit where credit is due.
+
+## MapBox access token ##
+* Use public default key.
+* Add to `config.js` file and add config.js to `.gitignore`.
+* Add line to index.html to import `config.js` BEFORE main `logic.js`.
+    * e.g 
+    `<script type="text/javascript" src="config.js"></script>`
+
+## How to hide? ##
+### Create `.gitignore` ###
+* What happens if you load to github.io?
+
+## GeoJson ##
+* a list of features.
+* features.properties
+* properties object
+* mag:
+* geometry
+* coordiantes
+* https://stackoverflow.com/questions/32615391/parsing-geojson-with-leaflet-from-an-external-api
+## How to parse the geoJSON format ##
+* https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson
+* [GeoJSON](https://leafletjs.com/reference-1.6.0.html#geojson)
+> Represents a GeoJSON object or an array of GeoJSON objects. Allows you to parse GeoJSON data and display it on the map. Extends FeatureGroup.
+
+## Layer Groups ##
+* Make markers based on coordinates and bind popups in the same line.
+* Create a Layers Group from an array of markers.
+
+## Event handling ##
+* On zoom
+~~~ 
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+mymap.on('click', onMapClick);
+~~~
+
+> Each object has its own set of events — see documentation for details. The first argument of the listener function is an event object — it contains useful information about the event that happened. For example, map click event object (e in the example above) has latlng property which is a location at which the click occurred.
+
+## Preparing the HTML page ##
+* confirm leaflet src
+1. js AND
+2. CSS --> goes before js??
+
+## Map must have defined height ##
+* Can be in CSS.
+* e.g. 100%
+
+## Leaflet legend ##
+* [Interactive Choropleth Map](https://leafletjs.com/examples/choropleth/)
+* Helpful with color scale too.
+
+## tool tips ##
+* https://leafletjs.com/reference-1.6.0.html#map-zoom
+
+## Useful VS Code Extensions ##
+> Name: Live Server
+> Id: ritwickdey.liveserver
+> Description: Launch a development local Server with live reload feature for static & dynamic pages
+> Version: 5.6.1
+> Publisher: Ritwick Dey
+> VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
